29e171a427a7862a9724b4d0967a593ddd9c632a
BIGPEL <bigpel66@gmail.com>
2020-03-25 17:27:04 +0900

    next function testing

--- Udemy/The Complete Guide of NodeJS/node_integrated_solution/app.js
+++ Udemy/The Complete Guide of NodeJS/node_integrated_solution/app.js
@@ -2,14 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.use((request, response, next) => {
-  console.log("1번 동작");
+app.use("/", (request, response, next) => {
+  console.log("called");
   next();
 });
 
-app.use((requset, response, next) => {
-  console.log("2번 동작");
-  response.send('<h1>Hello from Express</h1>');
+app.use("/add-product", (requset, response, next) => {
+  response.send('<h1>The "Add" Product</h1>');
+});
+
+app.use("/", (request, response, next) => {
+  response.send("<h1>Hello from Express</h1>");
 });
 
 app.listen(3000);
