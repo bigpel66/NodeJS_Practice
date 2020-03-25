41dd99cdb29ce0a9a316da718510c665b8d6983f
BIGPEL <bigpel66@gmail.com>
2020-03-25 14:14:15 +0900

    express installed and middlewares

--- Udemy/The Complete Guide of NodeJS/node_integrated_solution/app.js
+++ Udemy/The Complete Guide of NodeJS/node_integrated_solution/app.js
@@ -1,7 +1,18 @@
 const http = require("http");
 
-const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes);
+const app = express();
+
+app.use((request, response, next) => {
+  console.log("1번 동작");
+  next();
+});
+
+app.use((requset, response, next) => {
+  console.log("2번 동작");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
