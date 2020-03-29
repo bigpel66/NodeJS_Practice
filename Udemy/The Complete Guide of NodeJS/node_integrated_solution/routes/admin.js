09963d5c7b198b66aef187721851b749b6f51485
BIGPEL <bigpel66@gmail.com>
2020-03-30 01:35:28 +0900

    template files

--- Udemy/The Complete Guide of NodeJS/node_integrated_solution/routes/admin.js
+++ Udemy/The Complete Guide of NodeJS/node_integrated_solution/routes/admin.js
@@ -6,14 +6,17 @@ const rootDir = require("../helpers/path");
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product
 router
   .get("/add-product", (requset, response, next) => {
     response.sendFile(path.join(rootDir, "views", "add-product.html"));
   })
   .post("/add-product", (request, response, next) => {
-    console.log(request.body);
+    products.push({ title: request.body.title });
     response.redirect("/");
   });
 
-module.exports = router;
+module.exports.router = router;
+module.exports.products = products;
--- Udemy/The Complete Guide of NodeJS/node_integrated_solution/routes/shop.js
+++ Udemy/The Complete Guide of NodeJS/node_integrated_solution/routes/shop.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require('../helpers/path');
+const rootDir = require("../helpers/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (request, response, next) => {
+  console.log(adminData.products);
   response.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
