4b7980dc908b4fc6d10c46188f673abfbc250d71
BIGPEL <bigpel66@gmail.com>
2020-03-30 11:16:34 +0900

    template layout added: need dynamic actions on the anchor

--- Udemy/The Complete Guide of NodeJS/node_integrated_solution/app.js
+++ Udemy/The Complete Guide of NodeJS/node_integrated_solution/app.js
@@ -17,7 +17,7 @@ app.use("/admin", adminRoutes.router);
 app.use(shopRoutes);
 
 app.use((request, response, next) => {
-  response.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  response.status(404).render('404');
 });
 
 app.listen(3000);
