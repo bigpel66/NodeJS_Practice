2b17012ddbdc95cdfaeb3a40ca5f7c8eb6287354
BIGPEL <bigpel66@gmail.com>
2020-04-01 13:24:59 +0900

    model

--- Udemy/The Complete Guide of NodeJS/node_integrated_solution/controllers/products.js
+++ Udemy/The Complete Guide of NodeJS/node_integrated_solution/controllers/products.js
@@ -1,9 +1,9 @@
-const products = [];
+const Product = require('../models/product');
 
 module.exports.getAddProduct = (requset, response, next) => {
-    response.render("add-product", {
-        path: "/admin/add-product",
-        pageTitle: "Add-Product",
+    response.render('add-product', {
+        path: '/admin/add-product',
+        pageTitle: 'Add-Product',
         activeAddProduct: true,
         productCSS: true,
         formsCSS: true
@@ -11,16 +11,19 @@ module.exports.getAddProduct = (requset, response, next) => {
 };
 
 module.exports.postAddProduct = (request, response, next) => {
-    products.push({ title: request.body.title });
-    response.redirect("/");
+    const product = new Product(request.body.title);
+    console.log(request.body.title);
+    console.log(product.title);
+    product.save();
+    response.redirect('/');
 };
 
 module.exports.getProducts = (request, response, next) => {
-    // console.log(adminData.products);
-    response.render("shop", {
+    const products = Product.fetchAll();
+    response.render('shop', {
         products: products,
-        pageTitle: "Shop",
-        path: "/",
+        pageTitle: 'Shop',
+        path: '/',
         hasProducts: products.length > 0,
         activeShop: true,
         productCSS: true
