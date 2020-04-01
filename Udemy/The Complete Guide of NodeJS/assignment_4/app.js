66760dc33cc017526a1e0aa84d6ac6e8624297fa
BIGPEL <bigpel66@gmail.com>
2020-04-01 10:27:24 +0900

    assignment_4 completed

--- Udemy/The Complete Guide of NodeJS/assignment_4/app.js
+++ Udemy/The Complete Guide of NodeJS/assignment_4/app.js
@@ -1,32 +1,42 @@
 const express = require("express");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts",
-    defaultLayout: "main-layout",
-    extname: "hbs"
-  })
-);
+const users = [];
 
+// app.engine(
+//   "hbs",
+//   expressHbs({
+//     layoutsDir: "views/layouts/",
+//     defaultLayout: "main-layout",
+//     extname: "hbs"
+//   })
+// );
+
+// app.set("view engine", "hbs");
 app.set("view engine", "pug");
-app.set("veiw engine", "hbs");
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/", (request, response, next) => {
-  response.render();
+  response.render("index", { pageTitle: "Add User" });
 });
 
 app.get("/users", (request, response, next) => {
-  response.render();
+  response.render("users", {
+    pageTitle: "Users",
+    users: users,
+    hasUsers: users.length > 0
+  });
 });
 
 app.post("/add-user", (request, response, next) => {
-  response.redirect("/");
+  users.push({ name: request.body.username });
+  response.redirect("/users");
 });
 
 app.listen(3000);
