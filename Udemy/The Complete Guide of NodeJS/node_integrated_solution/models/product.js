8bcc1f27a82e9cc18be7f4d239afe3c8dc2d9018
BIGPEL <bigpel66@gmail.com>
2020-04-02 15:40:56 +0900

    now products have random id

--- Udemy/The Complete Guide of NodeJS/node_integrated_solution/models/product.js
+++ Udemy/The Complete Guide of NodeJS/node_integrated_solution/models/product.js
@@ -24,6 +24,7 @@ module.exports = class Product {
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             console.log(products);
