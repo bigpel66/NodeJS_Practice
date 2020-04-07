5d860630a73c1a32854bf822d6f0939d02bd9dbf
BIGPEL <bigpel66@gmail.com>
2020-04-07 13:37:03 +0900

    editing products

--- Udemy/The Complete Guide of NodeJS/node_integrated_solution/controllers/admin.js
+++ Udemy/The Complete Guide of NodeJS/node_integrated_solution/controllers/admin.js
@@ -36,41 +36,64 @@ module.exports.getEditProduct = (request, response, next) => {
         response.redirect('/products');
     }
 
-    Product.findById(productId, (product) => {
-        if (!product) {
-            return response.redirect('/');
-        }
-        response.render('admin/edit-product', {
-            path: '/admin/edit-product',
-            pageTitle: 'Edit Product',
-            editing: editing,
-            product: product,
+    Product.findByPk(productId)
+        .then((product) => {
+            if (!product) {
+                return response.redirect('/');
+            }
+            response.render('admin/edit-product', {
+                path: '/admin/edit-product',
+                pageTitle: 'Edit Product',
+                editing: editing,
+                product: product,
+            });
+        })
+        .catch((error) => {
+            if (error) {
+                console.log(error);
+            }
         });
-    });
 };
 
 module.exports.postEditProduct = (request, response, next) => {
-    const product = new Product(
-        request.body.id,
-        request.body.title,
-        request.body.imageUrl,
-        request.body.description,
-        request.body.price
-    );
+    const productId = request.body.id;
+    const productTitle = request.body.title;
+    const productPrice = request.body.price;
+    const productImageUrl = request.body.imageUrl;
+    const productDescription = request.body.description;
 
-    product.save();
-
-    response.redirect('/admin/products');
+    Product.findByPk(productId)
+        .then((product) => {
+            product.title = productTitle;
+            product.price = productPrice;
+            product.imageUrl = productImageUrl;
+            product.description = productDescription;
+            return product.save();
+        })
+        .then((results) => {
+            response.redirect('/admin/products');
+        })
+        .catch((error) => {
+            if (error) {
+                console.log(error);
+            }
+        });
 };
 
 module.exports.getProducts = (request, response, next) => {
-    Product.fetchAll((products) => {
-        response.render('admin/products', {
-            products: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products',
+    Product.findAll()
+        .then((results) => {
+            response.render('admin/products', {
+                products: results,
+                pageTitle: 'Admin Products',
+                path: '/admin/products',
+            });
+        })
+        .catch((error) => {
+            if (error) {
+                console.log(error);
+            }
         });
-    });
 };
 
 module.exports.postDeleteProduct = (request, response, next) => {
