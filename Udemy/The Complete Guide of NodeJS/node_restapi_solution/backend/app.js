32b69882731ea0bca9c84b800f0bccbc8ac23536
BIGPEL <bigpel66@gmail.com>
2020-04-27 16:40:16 +0900

    error handling through formatError Function on GraphQL

--- Udemy/The Complete Guide of NodeJS/node_restapi_solution/backend/app.js
+++ Udemy/The Complete Guide of NodeJS/node_restapi_solution/backend/app.js
@@ -64,6 +64,21 @@ app.use(
         schema: graphqlSchema,
         rootValue: graphqlResolver,
         graphiql: true,
+        formatError(err) {
+            if (!err.originalError) {
+                return err;
+            }
+
+            const data = err.originalError.data;
+            const code = err.originalError.code || 500;
+            const message = err.message || 'An error occurred.';
+
+            return {
+                message: message,
+                code: code,
+                data: data,
+            };
+        },
     })
 );
 // DELETED TO USE GRAPHQL
