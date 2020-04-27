34ba9b14bb4cd12a76f4ceafcbbf3aae63a2c1a9
BIGPEL <bigpel66@gmail.com>
2020-04-28 03:25:45 +0900

    login graphql connected to the frontend

--- Udemy/The Complete Guide of NodeJS/node_restapi_solution/backend/graphql/resolvers.js
+++ Udemy/The Complete Guide of NodeJS/node_restapi_solution/backend/graphql/resolvers.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -42,5 +43,54 @@ module.exports = {
         const createdUser = await user.save();
 
         return { ...createdUser._doc, _id: createdUser._id.toString() };
+    },
+
+    login: async function ({ email, password }) {
+        const errors = [];
+        const user = await User.findOne({ email: email });
+
+        if (!validator.isEmail(email)) {
+            errors.push({ message: 'E-Mail is invalid.' });
+        }
+
+        if (
+            validator.isEmpty(password) ||
+            !validator.isLength(password, { min: 5 })
+        ) {
+            errors.push({ message: 'Password too short!' });
+        }
+
+        if (errors.length > 0) {
+            const error = new Error('Invalid input.');
+            error.data = errors;
+            error.code = 422;
+
+            throw error;
+        }
+
+        if (!user) {
+            const error = new Error('User not found.');
+            error.code = 401;
+            throw error;
+        }
+
+        const result = await bcrypt.compare(password, user.password);
+
+        if (!result) {
+            const error = new Error('Password is incorrect.');
+            error.code = 401;
+            throw error;
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id.toString(),
+                email: user.email,
+            },
+            'somesupersecretkeyvalue',
+            { expiresIn: '1h' }
+        );
+
+        return { token: token, userId: user._id.toString() };
     },
 };
