64ae53608a996999f6fe778993920d1796c46937
BIGPEL <bigpel66@gmail.com>
2020-04-28 14:08:55 +0900

    updating post by graphql

--- Udemy/The Complete Guide of NodeJS/node_restapi_solution/frontend/src/pages/Feed/Feed.js
+++ Udemy/The Complete Guide of NodeJS/node_restapi_solution/frontend/src/pages/Feed/Feed.js
@@ -269,6 +269,25 @@ class Feed extends Component {
                     `,
                 };
 
+                if (this.state.editPost) {
+                    graphqlQuery = {
+                        query: `
+                            mutation {
+                                updatePost(id: "${this.state.editPost._id}", postInput: {title: "${postData.title}", content: "${postData.content}", imageUrl: "${imageUrl}"}) {
+                                    _id
+                                    title
+                                    content
+                                    imageUrl
+                                    creator {
+                                        name
+                                    }
+                                    createdAt
+                                }
+                            }
+                        `,
+                    };
+                }
+
                 return fetch('http://localhost:8080/graphql', {
                     method: 'POST',
                     headers: {
@@ -296,13 +315,19 @@ class Feed extends Component {
                     throw new Error('Creating post failed.');
                 }
 
+                let resDataField = 'createPost';
+
+                if (this.state.editPost) {
+                    resDataField = 'updatePost';
+                }
+
                 const post = {
-                    _id: resData.data.createPost._id,
-                    title: resData.data.createPost.title,
-                    content: resData.data.createPost.content,
-                    imagePath: resData.data.createPost.imageUrl,
-                    creator: resData.data.createPost.creator,
-                    createdAt: resData.data.createPost.createdAt,
+                    _id: resData.data[resDataField]._id,
+                    title: resData.data[resDataField].title,
+                    content: resData.data[resDataField].content,
+                    imagePath: resData.data[resDataField].imageUrl,
+                    creator: resData.data[resDataField].creator,
+                    createdAt: resData.data[resDataField].createdAt,
                 };
 
                 this.setState((prevState) => {
