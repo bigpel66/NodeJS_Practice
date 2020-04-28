334fd774ea3175f36d5c669d991f9971aba24b98
BIGPEL <bigpel66@gmail.com>
2020-04-29 01:03:30 +0900

    login and signup also used variables keyword for dynamic valued variables

--- Udemy/The Complete Guide of NodeJS/node_restapi_solution/frontend/src/App.js
+++ Udemy/The Complete Guide of NodeJS/node_restapi_solution/frontend/src/App.js
@@ -74,13 +74,17 @@ class App extends Component {
         // })
         const graphqlQuery = {
             query: `
-                {
-                    login(email: "${authData.email}", password: "${authData.password}") {
+                query Login($email: String!, $password: String!){
+                    login(email: $email, password: $password) {
                         token
                         userId
                     }
                 }
             `,
+            variables: {
+                email: authData.email,
+                password: authData.password,
+            },
         };
         fetch('http://localhost:8080/graphql', {
             method: 'POST',
@@ -166,13 +170,18 @@ class App extends Component {
 
         const graphqlQuery = {
             query: `
-              mutation {
-                createUser(userInput: {email: "${authData.signupForm.email.value}", name:"${authData.signupForm.name.value}", password:"${authData.signupForm.password.value}"}) {
+              mutation CreateUser($email: String!, $name: String!, $password: String!){
+                createUser(userInput: {email: $email, name: $name, password: $password}) {
                   _id
                   email
                 }
               }
             `,
+            variables: {
+                email: authData.signupForm.email.value,
+                name: authData.signupForm.name.value,
+                password: authData.signupForm.password.value,
+            },
         };
 
         fetch('http://localhost:8080/graphql', {
