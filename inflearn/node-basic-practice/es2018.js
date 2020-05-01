6db5843896d892a34b1038d6893ca2cf197a07c0
BIGPEL <bigpel66@gmail.com>
2020-05-02 01:02:33 +0900

    arrow function

--- inflearn/node-basic-practice/es2018.js
+++ inflearn/node-basic-practice/es2018.js
@@ -76,6 +76,48 @@
 // ARROW Function
 // ---------------------------------------------------------------------------
 // BEFORE ES6
+// var add = function (x, y) {
+//     return x + y;
+// };
+// var result = add(2, 3);
+// console.log(result);
+
+// AFTER ES6
+// let add = (x, y) => {
+//     return x + y;
+// };
+// let result = add(2, 3);
+// console.log(result);
+
+// THIS ON FUNCTION () {}
+// var relationship = {
+// name: 'J',
+// friends: ['S', 'K', 'L'],
+//     logFriends: function () {
+// var that = this;
+//         this.friends.forEach(function (friend) {
+// console.log(that.name, friend); // function 내부의 this와 외부의 this가 서로 다르기 때문에 that에 넣어서 사용한다.
+//         });
+//     },
+// };
+// relationship.logFriends();
+
+// THIS ON ARROW FUNCTION
+// const relationship = {
+//     name: 'J',
+//     friends: ['S', 'K', 'L'],
+//     logFriends() {
+//         this.friends.forEach((friend) => {
+//             console.log(this.name, friend); // Arrow Function 덕에 내부의 this와 외부의 this가 서로 동일하게 취급된다.
+//         });
+//     },
+// };
+// relationship.logFriends();
+// ---------------------------------------------------------------------------
+
+// DESTRUCTURING
+// ---------------------------------------------------------------------------
+// BEFORE ES6
 
 // AFTER ES6
 // ---------------------------------------------------------------------------
