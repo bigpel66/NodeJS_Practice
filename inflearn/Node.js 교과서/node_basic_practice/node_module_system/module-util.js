c02dd507f45e6364e013ae691d32e2cd08752dc4
BIGPEL <bigpel66@gmail.com>
2020-05-04 15:49:08 +0900

    deprecate, promisify from util

---
+++ inflearn/Node.js 교과서/node_basic_practice/node_module_system/module-util.js
@@ -0,0 +1,35 @@
+const util = require('util');
+const crypto = require('crypto');
+
+// DEPRECATE
+const doNotUseMe = util.deprecate((x, y) => {
+    console.log(x + y);
+}, '이 함수는 조만간 지원이 중단됩니다.');
+
+doNotUseMe(1, 2);
+
+// PROMISIFY
+// Before Promisify
+crypto.randomBytes(64, (err, buf) => {
+    const salt = buf.toString('base64');
+    crypto.pbkdf2('비밀번호', salt, 100000, 64, 'sha512', (err, key) => {
+        console.log('key from Callback Function: ', key.toString('base64'));
+    });
+});
+
+// After Promisify
+const randomBytesPromise = util.promisify(crypto.randomBytes);
+const pbkdf2Promise = util.promisify(crypto.pbkdf2);
+randomBytesPromise(64)
+    .then((result) => {
+        const salt = result.toString('base64');
+        return pbkdf2Promise('비밀번호', salt, 100000, 64, 'sha512');
+    })
+    .then((key) => {
+        console.log('key from Promisify Function: ', key.toString('base64'));
+    })
+    .catch((err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
