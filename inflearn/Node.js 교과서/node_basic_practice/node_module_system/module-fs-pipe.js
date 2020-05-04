ba64c8b2c0a8785d74734d4f451510635412347a
BIGPEL <bigpel66@gmail.com>
2020-05-04 17:57:23 +0900

    the resto f the fs module

--- inflearn/Node.js 교과서/node_basic_practice/node_module_system/module-fs-pipe.js
+++ inflearn/Node.js 교과서/node_basic_practice/node_module_system/module-fs-pipe.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const zlib = require('zlib');
 
+const zlibStream = zlib.createGzip();
 const readStream = fs.createReadStream('../files/READPIPE.txt');
 const writeStream = fs.createWriteStream('../files/WRITEPIPE.txt');
 
-readStream.pipe(writeStream);
+readStream.pipe(zlibStream).pipe(writeStream);
 
 const copyStream = fs.copyFile(
     '../files/READCOPYFILE.txt',
