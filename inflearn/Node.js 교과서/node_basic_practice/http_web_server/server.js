281b56b8a7ab2f287d9bec654a7464b40413ed26
BIGPEL <bigpel66@gmail.com>
2020-05-05 00:35:24 +0900

    cookie to the memory session example

--- inflearn/Node.js 교과서/node_basic_practice/http_web_server/server.js
+++ inflearn/Node.js 교과서/node_basic_practice/http_web_server/server.js
@@ -12,31 +12,46 @@ const parseCookies = (cookie = '') =>
             return acc;
         }, {});
 
-http.createServer((req, res) => {
-    const cookies = parseCookies(req.headers.cookie);
-    if (req.url.startsWith('/login')) {
-        const { query } = url.parse(req.url);
-        const { name } = qs.parse(query);
-        const expires = new Date();
-        expires.setMinutes(expires.getMinutes() + 5);
-        res.writeHead(302, {
-            Location: '/',
-            'Set-Cookie': `name=${encodeURIComponent(
-                name
-            )}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
-        });
-        res.end();
-    } else if (cookies.name) {
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.end(`${cookies.name}님 안녕하세요`);
-    } else {
-        fs.readFile('./server.html', (err, data) => {
-            if (err) {
-                throw err;
-            }
-            res.end(data);
-        });
-    }
-}).listen(8080, () => {
-    console.log('8080번 포트에서 서버 대기중입니다!');
+const session = {};
+
+const server = http
+    .createServer((req, res) => {
+        const cookies = parseCookies(req.headers.cookie);
+        if (req.url.startsWith('/login')) {
+            const { query } = url.parse(req.url);
+            const { name } = qs.parse(query);
+            const randomInt = +new Date();
+            const expires = new Date();
+            expires.setMinutes(expires.getMinutes() + 5);
+            session[randomInt] = {
+                name,
+                expires,
+            };
+            res.writeHead(302, {
+                Location: '/',
+                'Set-Cookie': `session=${randomInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+            });
+            res.end();
+        } else if (
+            cookies.session &&
+            session[cookies.session] &&
+            session[cookies.session].expires > new Date()
+        ) {
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.end(`${session[cookies.session].name}님 안녕하세요`);
+        } else {
+            fs.readFile('./server.html', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                res.end(data);
+            });
+        }
+    })
+    .listen(8080, () => {
+        console.log('8080번 포트에서 서버 대기중입니다!');
+    });
+
+server.on('listening', () => {
+    console.log('Port Number: 8080');
 });
