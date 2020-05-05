7767a053451560fe65a2c15c4a64f8543315fe08
BIGPEL <bigpel66@gmail.com>
2020-05-05 18:39:51 +0900

    router refactored as object

--- inflearn/Node.js 교과서/node_basic_practice/http_web_server/restServer.js
+++ inflearn/Node.js 교과서/node_basic_practice/http_web_server/restServer.js
@@ -3,38 +3,37 @@ const fs = require('fs');
 
 const users = {};
 
-http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        if (req.url === '/') {
-            return fs.readFile('./restFront.html', (err, data) => {
+const router = {
+    get: {
+        '/': (req, res) => {
+            fs.readFile('./restFront.html', (err, data) => {
                 if (err) {
                     throw err;
                 }
                 res.end(data);
             });
-        } else if (req.url === '/about') {
-            return fs.readFile('./about.html', (err, data) => {
+        },
+        '/users': (req, res) => {
+            res.end(JSON.stringify(users));
+        },
+        '*': (req, res) => {
+            fs.readFile(`.${req.url}`, (err, data) => {
                 if (err) {
-                    throw err;
+                    res.writeHead(404, 'NOT FOUND');
+                    return res.end('NOT FOUND');
                 }
-                res.end(data);
+                return res.end(data);
             });
-        } else if (req.url === '/users') {
-            return res.end(JSON.stringify(users));
-        }
-        return fs.readFile(`.${req.url}`, (err, data) => {
-            if (err) {
-                res.writeHead(404, 'NOT FOUND');
-                return res.end('NOT FOUND');
-            }
-            return res.end(data);
-        });
-    } else if (req.method === 'POST') {
-        if (req.url === '/users') {
+        },
+    },
+    post: {
+        '/users': (req, res) => {
             let body = '';
+
             req.on('data', (data) => {
                 body += data;
             });
+
             return req.on('end', () => {
                 console.log('POST 본문(Body):', body);
                 const { name } = JSON.parse(body);
@@ -45,29 +44,38 @@ http.createServer((req, res) => {
                 });
                 res.end('등록 성공');
             });
-        }
-    } else if (req.method === 'PUT') {
-        if (req.url.startsWith('/users/')) {
+        },
+    },
+    patch: {},
+    put: {
+        '/users': (req, res) => {
             const key = req.url.split('/')[2];
             let body = '';
+
             req.on('data', (data) => {
                 body += data;
             });
+
             return req.on('end', () => {
                 console.log('PUT 본문(Body):', body);
                 users[key] = JSON.parse(body).name;
                 return res.end(JSON.stringify(users));
             });
-        }
-    } else if (req.method === 'DELETE') {
-        if (req.url.startsWith('/users/')) {
+        },
+    },
+    delete: {
+        '/users': (req, res) => {
             const key = req.url.split('/')[2];
             delete users[key];
             return res.end(JSON.stringify(users));
-        }
-    }
-    res.writeHead(404, 'NOT FOUND');
-    return res.end('NOT FOUND');
+        },
+    },
+};
+
+http.createServer((req, res) => {
+    const url = req.url.split('/');
+    const matchedUrl = router[req.method.toLowerCase()][`/${url[1]}`];
+    (matchedUrl || router[req.method.toLowerCase()]['*'])(req, res);
 }).listen(8080, () => {
     console.log('8080번 포트에서 서버 대기중입니다');
 });
