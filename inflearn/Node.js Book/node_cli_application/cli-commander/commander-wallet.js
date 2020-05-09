49c2b6a2489b754dfba993466dce52e1d37f1782
BIGPEL <bigpel66@gmail.com>
2020-05-10 01:21:13 +0900

    sequelize revenue, expense, balance

--- inflearn/Node.js Book/node_cli_application/cli-commander/commander-wallet.js
+++ inflearn/Node.js Book/node_cli_application/cli-commander/commander-wallet.js
@@ -4,6 +4,7 @@ const inquirer = require('inquirer');
 const chalk = require('chalk');
 
 const { version } = require('./package.json');
+const { sequelize, Wallet } = require('./models/index');
 
 program.version(version, '-v, --version').usage('[options]');
 
@@ -11,18 +12,55 @@ program
     .command('revenue <money> <description>')
     .usage('<money> <description>')
     .description('Record Revenue')
-    .action(() => {});
+    .action(async (money, description) => {
+        await sequelize.sync();
+        await Wallet.create({
+            money: parseInt(money, 10),
+            description,
+            type: 1,
+        });
+        console.log(chalk.green(`${money} Revenue Recorded`));
+        await sequelize.close();
+    });
 
 program
     .command('expense <money> <description>')
     .usage('<money> <description>')
     .description('Record Expense')
-    .action(() => {});
+    .action(async (money, description) => {
+        await sequelize.sync();
+        await Wallet.creat({
+            money: parseInt(money, 10),
+            description,
+            type: 0,
+        });
+        console.log(chalk.green(`${money} Expense Recorded`));
+        await sequelize.close();
+    });
 
 program
     .command('balance')
     .description('Show Balance')
-    .action(() => {});
+    .action(async () => {
+        await sequelize.sync();
+        const logs = await Wallet.findAll({});
+        const revenue = logs
+            .filter((log) => {
+                return log.type === 1;
+            })
+            .reduce((acc, cur) => {
+                return acc + cur.money;
+            }, 0);
+        const expense = logs
+            .filter((log) => {
+                return log.type === 0;
+            })
+            .reduce((acc, cur) => {
+                return acc + cur.money;
+            }, 0);
+        console.log(`Balance => ${revenue - expense}`);
+        await sequelize.close();
+    });
 
 program.command('*').action(() => {
     console.log('No Command Found');
