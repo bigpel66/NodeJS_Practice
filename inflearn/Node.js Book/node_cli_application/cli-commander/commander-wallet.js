77ca72410c62cb1819b11c6d8b3fe218e180835e
BIGPEL <bigpel66@gmail.com>
2020-05-10 03:06:52 +0900

    account book finished

--- inflearn/Node.js Book/node_cli_application/cli-commander/commander-wallet.js
+++ inflearn/Node.js Book/node_cli_application/cli-commander/commander-wallet.js
@@ -6,6 +6,38 @@ const chalk = require('chalk');
 const { version } = require('./package.json');
 const { sequelize, Wallet } = require('./models/index');
 
+const dbCreateOperation = async (money, description, type) => {
+    await sequelize.sync();
+    await Wallet.create({
+        money: parseInt(money, 10),
+        description,
+        type: type,
+    });
+    await sequelize.close();
+};
+
+const dbReadOperation = async () => {
+    await sequelize.sync();
+    const logs = await Wallet.findAll({});
+    const revenue = logs
+        .filter((log) => {
+            return log.type === true;
+        })
+        .reduce((acc, cur) => {
+            return acc + cur.money;
+        }, 0);
+    const expense = logs
+        .filter((log) => {
+            return log.type === false;
+        })
+        .reduce((acc, cur) => {
+            return acc + cur.money;
+        }, 0);
+    await sequelize.close();
+
+    return revenue - expense;
+};
+
 program.version(version, '-v, --version').usage('[options]');
 
 program
@@ -13,14 +45,8 @@ program
     .usage('<money> <description>')
     .description('Record Revenue')
     .action(async (money, description) => {
-        await sequelize.sync();
-        await Wallet.create({
-            money: parseInt(money, 10),
-            description,
-            type: 1,
-        });
+        await dbCreateOperation(money, description, 1);
         console.log(chalk.green(`${money} Revenue Recorded`));
-        await sequelize.close();
     });
 
 program
@@ -28,42 +54,66 @@ program
     .usage('<money> <description>')
     .description('Record Expense')
     .action(async (money, description) => {
-        await sequelize.sync();
-        await Wallet.creat({
-            money: parseInt(money, 10),
-            description,
-            type: 0,
-        });
+        await dbCreateOperation(money, description, 0);
         console.log(chalk.green(`${money} Expense Recorded`));
-        await sequelize.close();
     });
 
 program
     .command('balance')
     .description('Show Balance')
     .action(async () => {
-        await sequelize.sync();
-        const logs = await Wallet.findAll({});
-        const revenue = logs
-            .filter((log) => {
-                return log.type === 1;
-            })
-            .reduce((acc, cur) => {
-                return acc + cur.money;
-            }, 0);
-        const expense = logs
-            .filter((log) => {
-                return log.type === 0;
-            })
-            .reduce((acc, cur) => {
-                return acc + cur.money;
-            }, 0);
-        console.log(`Balance => ${revenue - expense}`);
-        await sequelize.close();
+        const diff = await dbReadOperation();
+        console.log(`Balance => ${diff}`);
     });
 
-program.command('*').action(() => {
-    console.log('No Command Found');
+program.action(async (cmd, args) => {
+    if (args) {
+        console.error(chalk.bold.red('No Command Found'));
+        program.help();
+    } else {
+        const answers = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'type',
+                message: 'Command?',
+                choices: ['revenue', 'expense', 'balance'],
+            },
+            {
+                type: 'input',
+                name: 'money',
+                message: 'Amount?',
+                default: 0,
+                when: (answers) => {
+                    return ['revenue', 'expense'].includes(answers.type);
+                },
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Description?',
+                default: '.',
+                when: (answers) => {
+                    return ['revenue', 'expense'].includes(answers.type);
+                },
+            },
+            {
+                type: 'confirm',
+                name: 'confirm',
+                message: 'Confirm?',
+            },
+        ]);
+
+        if (answers.confirm) {
+            if (answers.type === 'revenue') {
+                await dbCreateOperation(answers.money, answers.description, 1);
+            } else if (answers.type === 'expense') {
+                await dbCreateOperation(answers.money, answers.description, 0);
+            } else {
+                const diff = await dbReadOperation();
+                console.log(`Balance => ${diff}`);
+            }
+        }
+    }
 });
 
 program.parse(process.argv);
