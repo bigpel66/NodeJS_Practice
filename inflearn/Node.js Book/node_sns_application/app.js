0f5b094a1bd463bbdf5aaf41914034fcb80fb3ee
BIGPEL <bigpel66@gmail.com>
2020-05-10 17:20:10 +0900

    dot env file created, password replaced

--- inflearn/Node.js Book/node_sns_application/app.js
+++ inflearn/Node.js Book/node_sns_application/app.js
@@ -6,24 +6,26 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const { sequelize } = require('./models/index');
 
+require('dotenv').config();
+
 const app = express();
 
 sequelize.sync();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT);
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('snsapplication'));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
     session({
         resave: false,
         saveUninitialized: false,
-        secret: 'snsapplication',
+        secret: process.env.COOKIE_SECRET,
         cookie: {
             httpOnly: true,
             secure: false,
