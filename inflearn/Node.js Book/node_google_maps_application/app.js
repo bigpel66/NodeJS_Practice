7583aab819090306bbb5f1cec96909171c26c505
BIGPEL <bigpel66@gmail.com>
2020-05-20 01:49:12 +0900

    google maps application init

---
+++ inflearn/Node.js Book/node_google_maps_application/app.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const morgan = require('morgan');
+const flash = require('connect-flash');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+const accessStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+    flags: 'a',
+});
+
+app.set('port', process.env.PORT || 8080);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(morgan('dev', { stream: accessStream }));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+    session({
+        resave: false,
+        saveuninitialized: false,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+        },
+    })
+);
+app.use(flash());
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+const connect = async () => {
+    try {
+        mongoose.set('debug', false);
+
+        await mongoose.connect(process.env.MONGO_URL);
+
+        mongoose.connection.on('error', (err) => {
+            console.error('MONGO DB ERROR', err);
+        });
+        mongoose.connection.on('disconnected', () => {
+            console.error('MONGO DB DISCONNECTED\nRETRYING TO CONNECT');
+            connect();
+        });
+
+        require('./schemas/favorite');
+        require('./schemas/history');
+
+        app.listen(app.get('port'), () => {
+            console.log(`Server Running on ${app.get('port')}!`);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+connect();
