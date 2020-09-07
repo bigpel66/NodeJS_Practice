618f7bdcec74091a2aa94137db549693c4f2f967
BIGPEL <bigpel66@gmail.com>
2020-09-08 00:47:36 +0900

    redis connection and caching: need to redis instance move

--- Udemy/NodeJS Advanced Concepts/advanced_node_starter/routes/blogRoutes.js
+++ Udemy/NodeJS Advanced Concepts/advanced_node_starter/routes/blogRoutes.js
@@ -1,38 +1,64 @@
+const util = require('util');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
 
-module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
+module.exports = (app) => {
+    app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+        const blog = await Blog.findOne({
+            _user: req.user.id,
+            _id: req.params.id,
+        });
+
+        res.send(blog);
     });
 
-    res.send(blog);
-  });
+    app.get('/api/blogs', requireLogin, async (req, res) => {
+        const redis = require('redis');
+        const redisUrl = 'redis://127.0.0.1:6379';
+        const redisClient = redis.createClient(redisUrl);
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+        // 해당 Query가 Redis에 Caching되어 있는지 확인
 
-    res.send(blogs);
-  });
+        // // const cachedBlogs = client.get(req.user.id, () => {});
+        // // 위 구문은 바람직 하지 않음 Promisify가 필요
+        redisClient.get = util.promisify(redisClient.get);
+        const cachedBlogs = await redisClient.get(req.user.id);
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
+        // Caching되어 있다면, Redis로부터 즉시 응답 수신
+        // JSON.parse
+        if (cachedBlogs) {
+            console.log('Serving FROM REDIS');
+            return res.send(JSON.parse(cachedBlogs));
+        }
 
-    const blog = new Blog({
-      title,
-      content,
-      _user: req.user.id
+        // Caching되어 있지 않다면, Query를 DB로 직접 송신
+        // Redis Caching 데이터 생성 필요
+        // JSON.stringify
+
+        const blogs = await Blog.find({ _user: req.user.id });
+
+        console.log('Serving FROM MONGO');
+        res.send(blogs);
+
+        redisClient.set(req.user.id, JSON.stringify(blogs));
     });
 
-    try {
-      await blog.save();
-      res.send(blog);
-    } catch (err) {
-      res.send(400, err);
-    }
-  });
+    app.post('/api/blogs', requireLogin, async (req, res) => {
+        const { title, content } = req.body;
+
+        const blog = new Blog({
+            title,
+            content,
+            _user: req.user.id,
+        });
+
+        try {
+            await blog.save();
+            res.send(blog);
+        } catch (err) {
+            res.send(400, err);
+        }
+    });
 };
