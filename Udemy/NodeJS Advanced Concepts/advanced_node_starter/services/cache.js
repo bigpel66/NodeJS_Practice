875fb2777827efd840bb1d9a4a269e276a2d87cd
BIGPEL <bigpel66@gmail.com>
2020-09-08 16:05:17 +0900

    caching on monkey patched exec function

--- Udemy/NodeJS Advanced Concepts/advanced_node_starter/services/cache.js
+++ Udemy/NodeJS Advanced Concepts/advanced_node_starter/services/cache.js
@@ -1,8 +1,43 @@
+const util = require('util');
 const mongoose = require('mongoose');
+const redis = require('redis');
+const redisUrl = 'redis://127.0.0.1:6379';
+const redisClient = redis.createClient(redisUrl);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
+redisClient.get = util.promisify(redisClient.get);
+
+mongoose.Query.prototype.exec = async function () {
     // caching on redis here
 
-    return exec.apply(this, arguments);
+    // Cache Key로 활용
+    // console.log(this.getQuery());
+    // console.log(this.mongooseCollection.name);
+
+    // JSON.stringify 필요
+    const key = JSON.stringify(
+        Object.assign({}, this.getQuery(), {
+            collection: this.mongooseCollection.name,
+        })
+    );
+
+    // REDIS에 Key가 있는지 확인 후, 있으면 Value 리턴
+    const cacheValue = await redisClient.get(key);
+
+    if (cacheValue) {
+        const doc = JSON.parse(cacheValue);
+
+        return Array.isArray(doc)
+            ? doc.map((d) => {
+                  return new this.model(d);
+              })
+            : new this.model(doc);
+    }
+
+    // 없다면 Query 수행하고 해당 값을 REDIS에 저장
+    const result = await exec.apply(this, arguments);
+
+    redisClient.set(key, JSON.stringify(result));
+
+    return result;
 };
