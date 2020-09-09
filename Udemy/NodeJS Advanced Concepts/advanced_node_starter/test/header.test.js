ee9a9f7483d7ca3989abc837820a14b734898d45
BIGPEL <bigpel66@gmail.com>
2020-09-09 22:05:28 +0900

    test refactored in beforeEach

--- Udemy/NodeJS Advanced Concepts/advanced_node_starter/test/header.test.js
+++ Udemy/NodeJS Advanced Concepts/advanced_node_starter/test/header.test.js
@@ -14,19 +14,43 @@
 // 오타가 자주나므로 재차 확인한다.
 const puppeteer = require('puppeteer');
 
+let browser, page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        //  headless 값이 true일 경우, GUI에 대한 제공을 하지 않는다.
+        headless: false,
+    });
+
+    page = await browser.newPage();
+
+    await page.goto('localhost:3000');
+});
+
 test('Launch Chromium Browswer', async () => {
     // 1. launch 함수 내에 Empty Object를 잊지 말 것
     //      (Browser 생성에 이용할 인자를 추후에 추가함)
     // 2. 오타 내지 말 것
     // 생성된 browser는 Running Browser이며, Browser Object이다.
-    const browser = await puppeteer.launch({
-        //  headless 값이 true일 경우, GUI에 대한 제공을 하지 않는다.
-        headless: false,
-    });
+    // Refactored
+    // const browser = await puppeteer.launch({
+    //     //  headless 값이 true일 경우, GUI에 대한 제공을 하지 않는다.
+    //     headless: false,
+    // });
 
     // Browser Object의 newPage 함수를 통해 새로운 Page를 생성할 수 있다.
     // 생성된 page는 Running Tab이며, Page Object이다.
-    // Page에서의 특정 요소를 클릭하게 할 수 있다. 
+    // Page에서의 특정 요소를 클릭하게 할 수 있다.
     // Page를 다른 Page로 Navigate 시킬 수 있다.
-    const page = await browser.newPage();
+    // Refactored
+    // const page = await browser.newPage();
+
+    // Refactored
+    // await page.goto('localhost:3000');
+
+    const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
+
+    await browser.close();
 });
