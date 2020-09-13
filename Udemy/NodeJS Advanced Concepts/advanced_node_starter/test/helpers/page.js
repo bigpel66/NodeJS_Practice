257e52869be41a8115a72fda1f0c94c44b6b7e7a
BIGPEL <bigpel66@gmail.com>
2020-09-13 20:31:52 +0900

    close function on browser

--- Udemy/NodeJS Advanced Concepts/advanced_node_starter/test/helpers/page.js
+++ Udemy/NodeJS Advanced Concepts/advanced_node_starter/test/helpers/page.js
@@ -34,6 +34,11 @@ class CustomPage {
 
         await this.page.goto('localhost:3000');
     }
+
+    // 해당 함수를 만들기 싫다면, Proxy의 get 함수에서 browser와 page의 순서를 바꾼다.
+    close() {
+        this.browser.close();
+    }
 }
 
 module.exports = CustomPage;
